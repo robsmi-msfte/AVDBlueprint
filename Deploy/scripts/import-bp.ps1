a26b8e6c08d5f814225d41cc85fd28be599bb222
sandude-ms <robsmi_ms@hotmail.com>
2021-02-09 15:06:48 -0500

    assignment files

--- Deploy/scripts/import-bp.ps1
+++ Deploy/scripts/import-bp.ps1
@@ -1,14 +1,13 @@
 $jConfig = Get-Content "./run.config.json" | ConvertFrom-Json
-
 $tenantID = $jConfig.args.tenantID
 $subID = $jConfig.args.subscriptionID
 $bpPath = $jConfig.args.blueprintPath
 $bpName = 'WVDBlueprint'
 
-If (!(Get-AzContext)) {
+If (!(Get-AzContext)) {​​
     Write-Host "Please login to your Azure account"
     Connect-AzAccount -Tenant $tenantID -Subscription $subID
-}
+}​​
 
 Import-AzBlueprintWithArtifact -Name $bpName -InputPath $bpPath -SubscriptionId $subID
 
