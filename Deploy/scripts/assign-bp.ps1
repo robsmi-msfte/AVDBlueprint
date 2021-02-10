47a02a8e5cdf2ee664ee56dfb3befd444c3028e1
Steve Downs <steve.downs@microsoft.com>
2021-02-09 20:18:00 -0500

    Updated deployment script
    UPdated DAuser and DNS deployment script to use newer PoSH and forceupdatetag.

--- Deploy/scripts/assign-bp.ps1
+++ Deploy/scripts/assign-bp.ps1
@@ -1,5 +1,26 @@
-$jConfig = Get-Content "./run.config.json" | ConvertFrom-Json
-$tenantID = $jConfig.args.tenantID
-$subID = $jConfig.args.subscriptionID
-$assignment = $jConfig.args.assignmentFile
-.\assign-bp.ps1 -tenantID $tenantID -subscriptionID $subID -assignFile $assignment
+[CmdletBinding(SupportsShouldProcess=$true)]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string] $tenantID,
+
+    [Parameter(Mandatory=$true)]
+    [string] $subscriptionID,
+
+    [Parameter(Mandatory=$true)]
+    [string] $bpName,
+
+    [Parameter(Mandatory=$false)]
+    [string] $assignFile = '..\assignments\assign_default.json'
+)
+
+$version =(Get-Date -Format "yyyyMMddHHmmss").ToString()
+$assignmentName = $bpName + '_' + $version
+
+If (!(Get-AzContext)) {
+    Write-Host "Please login to your Azure account"
+    Connect-AzAccount -Tenant $tenantID -Subscription $subID
+}
+
+$bpAssignment = New-AzBlueprintAssignment -Name $assignmentName -SubscriptionId $subscriptionID -AssignmentFile $assignFile
+
+Write-Output $bpAssignment
