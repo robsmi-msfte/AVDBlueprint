70d85abc03d86166f042cba212b708bb692614dd
robsmi-msfte <61853161+robsmi-msfte@users.noreply.github.com>
2021-08-17 18:47:53 -0400

    Update createAllAVDUsers.ps1
    Removing reference here, to the script that assigns users to the AVD application group.  Instead, the 'AVD Users' AAD group is going to be added to the AVD DAG, in the script that is run from the management VM that configures things and sets up group policy.

--- scripts/createAllAVDUsers.ps1
+++ scripts/createAllAVDUsers.ps1
@@ -15,13 +15,7 @@ for ($i = 1 ; $i -le $totalUsers ; $i++) {
     Write-host "Creating $userPrincipalName"
     
     if ($null -eq (Get-AzADUser -UserPrincipalName $userPrincipalName)) {
-        Start-Sleep -s 2
         .\addADuser.ps1 -displayName "$displayName" -userPrincipalName "$userPrincipalName" -keyVault $keyvault -forcePasswordChange $forcePasswordChange
-        Start-Sleep -s 2
     }
-
     .\assignADGroup.ps1 -groupName "$adGroup" -userPrincipalName "$userPrincipalName"
-    Start-Sleep -s 2
-    .\assignAVDRole.ps1 -upn "$userPrincipalName" -roleName "$avdRolename" -appGroupName "$avdAppGroup" -appGroupRG "$appGroupRG"
-    
 }
