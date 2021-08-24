a673b6f8919cc0ccd08639381facb145df3c2673
Robert Smith <robsmi_ms@hotmail.com>
2021-08-24 17:58:10 -0400

    updated KV purge code

--- Examples & Samples/Remove-AzAvdBpDeployment.ps1
+++ Examples & Samples/Remove-AzAvdBpDeployment.ps1
@@ -48,7 +48,10 @@ Param(
     [switch] $DisableLogExport,
     #Path to the folder where the target environment's logs should be copied
     [Parameter(Mandatory=$true, ParameterSetName="LogExport")]
-    [string] $LogPath
+    [string] $LogPath,
+    #Switch to purge key vault, not just soft delete
+    [Parameter()]
+    [Switch] $PurgeKeyVault
 )
 
 $RemovalScope = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -like "$($Prefix)*"} 
@@ -106,6 +109,13 @@ $RemovalScope | ForEach-Object {
         }
     }
    
+    if ($PurgeKeyVault)
+    {
+        $KeyVaultToPurge = Get-AzKeyVault -ResourceGroupName $RemovalScope.ResourceGroupName
+        Write-Verbose "Found '$($KeyVaultToPurge.VaultName)' Key Vault"
+        Remove-AzKeyVault -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
+        Remove-AzKeyVault -InRemovedState -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
+    }
 
     if($PSCmdlet.ShouldProcess($_.ResourceGroupName, "Remove ResourceGroup")){
         $_ | Remove-AzResourceGroup -Force -AsJob
