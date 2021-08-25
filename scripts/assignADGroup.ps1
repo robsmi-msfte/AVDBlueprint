168123cfb95bdd5a3a189f94bb29dd24a143264e
Robert Smith <robsmi_ms@hotmail.com>
2021-08-25 15:37:29 -0400

    Changed the test condition

--- scripts/assignADGroup.ps1
+++ scripts/assignADGroup.ps1
@@ -1,12 +1,12 @@
 param ($groupName, $userPrincipalName)
 Write-Host "Adding UPN ($userPrincipalName) to group ($groupName)"
 
-if ($null -eq (Get-AzADGroup -DisplayName "$groupName")) {
+if (-Not (Get-AzADGroup -DisplayName "$groupName")) {
     $mailNickname = $groupName -replace '[\W]',''
     New-AzADGroup -DisplayName "$groupName" -MailNickname $mailNickname
 }
 
-if ($null -eq (Get-AzADGroupMember -GroupDisplayName "$groupName" | Where-Object {$_.UserPrincipalName -eq $userPrincipalName})) {
+if (-Not (Get-AzADGroupMember -GroupDisplayName "$groupName" | Where-Object {$_.UserPrincipalName -eq $userPrincipalName})) {
     $parameters = @{
         TargetGroupDisplayName              =  "$groupName"
         MemberUserPrincipalName             =  $userPrincipalName
