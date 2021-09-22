b7a390b55d6af0be272c14c1e49e99b060af03f6
Robert Smith <robsmi_ms@hotmail.com>
2021-09-22 07:11:52 -0400

    UD remove script to install modules if missing

--- Examples & Samples/Remove-AzAvdBpDeployment.ps1
+++ Examples & Samples/Remove-AzAvdBpDeployment.ps1
@@ -54,6 +54,22 @@ Param(
     [switch] $PurgeKeyVault
 )
 
+if (-not(Get-Module -Name Az.Keyvault)) {
+    Install-Module 'Az.Keyvault' -Force
+}
+if (-not(Get-Module -Name Az.Storage)) {
+    Install-Module 'Az.Storage' -Force
+}
+if (-not(Get-Module -Name Az.Resources)) {
+    Install-Module 'Az.Resources' -Force
+}
+if (-not(Get-Module -Name Az.OperationalInsights)) {
+    Install-Module 'Az.OperationalInsights' -Force
+}
+if (-not(Get-Module -Name AzureAD)) {
+    Install-Module 'AzureAD' -Force
+}
+
 $RemovalScope = Get-AzResourceGroup | Where-Object {$_.ResourceGroupName -like "$($Prefix)*"} 
 Write-Verbose "Found $($RemovalScope.count) Resource Groups"
 
@@ -111,9 +127,6 @@ $RemovalScope | ForEach-Object {
    
     Write-Verbose "Now purging key vault"
     if ($PurgeKeyVault) {
-        if (-not(Get-Module -Name Az.Keyvault)) {
-            Install-Module 'Az.Keyvault' -Force
-        }
         $KeyVaultToPurge = Get-AzKeyVault -ResourceGroupName $RemovalScope.ResourceGroupName
         Write-Verbose "Found '$($KeyVaultToPurge.VaultName)' Key Vault"
         Remove-AzKeyVault -VaultName $KeyVaultToPurge.VaultName -Location $RemovalScope.Location -Force
